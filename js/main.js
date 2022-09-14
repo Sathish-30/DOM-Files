a4ae2251ccc15eb2b2b77a45475e7a89f4bd93ab
Sathish S <sathish.amsaravanan@gmail.com>
2022-09-14 06:06:42 +0530

    File added
    Created a Navbar using Simple react

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//import "src/main.css";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div class="container">
+    <nav class="navbar">
+      <ol class="nav-list">
+        <li class="nav-element">Home</li>
+        <li class="nav-element">About</li>
+        <li class="nav-element">Education</li>
+        <li class="nav-element">contact</li>
+      </ol>
+    </nav>
+  </div>
+);
